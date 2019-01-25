5167ee7fa4a56df2af9d1dc205fe0bf358cdd97f
cyril.savary.42@gmail.com <fzefre@fezrf.com>
2019-01-25 21:58:54 +0100

    des trucs

---
+++ SmasHome/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    private bool onFloor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        onFloor = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var speed = 3;
+        var horizontal = Input.GetAxis("Horizontal");
+        if (horizontal != 0)
+        {
+            transform.localPosition = transform.localPosition + new Vector3(horizontal * speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetButtonDown("Jump") && onFloor)
+        {
+            rigidbody.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+            onFloor = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        if (collision2D.gameObject.tag == "Floor")
+        {
+            onFloor = true;
+        }
+    }
+}
