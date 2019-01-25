bddffa04c53e1ce12559f829e6f3692340e62dbe
cyril.savary.42@gmail.com <fzefre@fezrf.com>
2019-01-25 22:17:30 +0100

    2 joueurs

--- SmasHome/Assets/Scripts/PlayerController.cs
+++ SmasHome/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rigidbody;
     private bool onFloor;
 
+    public int PlayerNumber;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,13 +20,13 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         var speed = 3;
-        var horizontal = Input.GetAxis("Horizontal");
+        var horizontal = Input.GetAxis("Horizontal" + PlayerNumber);
         if (horizontal != 0)
         {
             transform.localPosition = transform.localPosition + new Vector3(horizontal * speed * Time.deltaTime, 0, 0);
         }
 
-        if (Input.GetButtonDown("Jump") && onFloor)
+        if (Input.GetButtonDown("Jump" + PlayerNumber) && onFloor)
         {
             rigidbody.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
             onFloor = false;
@@ -33,7 +35,7 @@ public class PlayerController : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision2D)
     {
-        if (collision2D.gameObject.tag == "Floor")
+        if (collision2D.gameObject.tag == "Floor" || collision2D.gameObject.tag == "Player")
         {
             onFloor = true;
         }
