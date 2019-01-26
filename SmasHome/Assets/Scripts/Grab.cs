832c2943150695b3645ac3594ed04bc42f156999
cyril.savary.42@gmail.com <fzefre@fezrf.com>
2019-01-26 02:59:29 +0100

    grab

---
+++ SmasHome/Assets/Scripts/Grab.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grab : MonoBehaviour
+{
+
+    public List<GameObject> CanGrab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CanGrab = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- SmasHome/Assets/Scripts/PlayerController.cs
+++ SmasHome/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rigidbody;
     private bool onFloor;
     private bool front;
+    private GameObject grabbed;
+
 
     public int PlayerNumber;
 
@@ -16,19 +18,30 @@ public class PlayerController : MonoBehaviour
         rigidbody = GetComponent<Rigidbody2D>();
         onFloor = true;
         front = true;
+        grabbed = null;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         var speed = 3;
+
+        //deplacement
         var horizontal = Input.GetAxis("Horizontal" + PlayerNumber);
-        var vertical = Input.GetAxis("Vertical" + PlayerNumber);
         if (horizontal != 0)
         {
             transform.localPosition = transform.localPosition + new Vector3(horizontal * speed * Time.deltaTime, 0, 0);
         }
 
+        //Jump
+        if (Input.GetButtonDown("Jump" + PlayerNumber) && onFloor)
+        {
+            rigidbody.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+            onFloor = false;
+        }
+
+        //changement de plan
+        var vertical = Input.GetAxis("Vertical" + PlayerNumber);
         if (vertical != 0)
         {
             if (vertical > 0.9)
@@ -51,10 +64,32 @@ public class PlayerController : MonoBehaviour
             gameObject.layer = LayerMask.NameToLayer("PlayerBack");
         }
 
-        if (Input.GetButtonDown("Jump" + PlayerNumber) && onFloor)
+        //grab
+        if (Input.GetButtonDown("Grab" + PlayerNumber))
         {
-            rigidbody.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
-            onFloor = false;
+            if (grabbed == null)
+            {
+                var grab = gameObject.transform.Find("Grab").GetComponent<Grab>();
+                if (grab.CanGrab.Count > 0)
+                {
+                    grabbed = grab.CanGrab[0];
+                    grabbed.transform.parent = grab.transform;
+                    grabbed.transform.localPosition = Vector3.zero;
+                    grabbed.GetComponent<BoxCollider2D>().enabled = false;
+                    grabbed.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+                }
+            } else
+            {
+                grabbed.transform.parent = null;
+                grabbed.GetComponent<BoxCollider2D>().enabled = true;
+                grabbed.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+                grabbed.GetComponent<Rigidbody2D>().AddForce(new Vector2(-3, 1), ForceMode2D.Impulse);
+                grabbed = null;
+            }
+        }
+
+        if (grabbed != null){
+            grabbed.transform.localPosition = Vector3.zero;
         }
     }
 
