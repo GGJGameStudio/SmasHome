2250cb0d6a91401372b67ce2d6a03148bb236ac9
zao29 <nicolsa.bellot@gmail.com>
2019-01-26 18:32:35 +0100

    Music Manager for Menu scene

---
+++ SmasHome/Assets/Music/Sound/QuentinManager/musicManagerQuentin.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicManager : MonoBehaviour
+{
+    public AudioSource source;
+    [SerializeField] public List<AudioClip> clips;
+    bool pause = false;
+
+    public int nextClip = 0;
+
+    public void Start()
+    {
+        source = GetComponent<AudioSource>();
+
+        source.PlayOneShot(clips[nextClip]);
+
+    }
+
+    public void setNextClip(int index)
+    {
+        nextClip = index;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            source.Pause();
+            pause = true;
+        }
+
+
+            if (!source.isPlaying && !pause)
+        {
+            nextClip++;
+            if(nextClip >= clips.Count)
+            {
+                nextClip = 0;
+            }
+            source.PlayOneShot(clips[nextClip]);
+        }
+    }
+
+}
