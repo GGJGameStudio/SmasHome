19e7c347987efddb48bfc5eef9dcfe72cfc5574a
cyril.savary.42@gmail.com <fzefre@fezrf.com>
2019-01-26 14:54:21 +0100

    score

---
+++ SmasHome/Assets/Scripts/UIController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    private GameObject canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = GameObject.FindGameObjectWithTag("Canvas");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        for (int i = 1; i <= 4; i++)
+        {
+            var player = GameObject.Find("Player" + i);
+            
+            if (player != null)
+            {
+                canvas.transform.Find("Age" + i).GetComponent<Text>().text = player.GetComponent<PlayerController>().Age.ToString();
+            }
+        }
+    }
+}
