6acb988aa8b6b6e79c320b95a5d7ce10484380d9
Louis. Polart <you@example.com>
2019-01-27 01:31:06 +0100

    Fondu

---
+++ SmasHome/Assets/Scripts/Fondu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fondu : MonoBehaviour
+{
+    public SpriteRenderer sprite;
+    private float startTime = 0.0f;
+    private bool started = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float delta = (Time.realtimeSinceStartup - startTime) * 1.5f;
+        if (started && delta < 3.14f)
+        {
+            float alpha = 0.0f;
+            alpha = Mathf.Sin(delta);
+            sprite.material.SetFloat("_Alpha", alpha);
+        }
+        else if (started)
+        {
+            sprite.material.SetFloat("_Alpha", 0.0f);
+            started = false;
+        }
+    }
+
+    public void startFondu()
+    {
+        startTime = Time.realtimeSinceStartup;
+        started = true;
+    }
+}
