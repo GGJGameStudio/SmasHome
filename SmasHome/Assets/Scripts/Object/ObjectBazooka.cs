1e4ba3582a684a2c4bbf571fa342113d5c9f6cfa
cyril.savary.42@gmail.com <fzefre@fezrf.com>
2019-01-27 16:01:26 +0100

    bazooka

---
+++ SmasHome/Assets/Scripts/Object/ObjectBazooka.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectBazooka : ObjectBasic
+{
+    public Object Rocket;
+
+    private bool rightdir;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    public override void Strike(bool rightdir)
+    {
+        var rocket = Instantiate(Rocket, transform.position, Quaternion.identity) as GameObject;
+        rocket.GetComponent<Rigidbody2D>().gravityScale = 0;
+        rocket.GetComponent<Rigidbody2D>().AddForce(new Vector2((rightdir ? 1 : -1) * 5, 0), ForceMode2D.Impulse);
+        rocket.GetComponent<ObjectBasic>().Flying = true;
+        rocket.GetComponent<ObjectBasic>().Owner = -1;
+        if (rightdir)
+        {
+            rocket.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        
+    }
+}
--- SmasHome/Assets/Scripts/Object/ObjectFireworks.cs
+++ SmasHome/Assets/Scripts/Object/ObjectFireworks.cs
@@ -29,18 +29,21 @@ public class ObjectFireworks : ObjectBasic
 
         var explosion = Instantiate(ExplosionEffect, transform.position, Quaternion.identity) as GameObject;
         var center = explosion.transform.position;
-        var hits = Physics2D.OverlapCircleAll(center, 1);
+        var hits = Physics2D.OverlapCircleAll(center, 1.5f);
         foreach(Collider2D col in hits)
         {
             var obj = col.gameObject;
             if (obj.tag == "Object" && obj != gameObject)
             {
-                obj.GetComponent<Collider2D>().enabled = true;
-                obj.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-                obj.layer = LayerMask.NameToLayer("Object");
-                obj.GetComponent<Rigidbody2D>().AddForce(5 * (col.gameObject.transform.position - center).normalized, ForceMode2D.Impulse);
-                obj.GetComponent<ObjectBasic>().Flying = true;
-                obj.GetComponent<ObjectBasic>().Owner = -1;
+                if (obj.GetComponent<ObjectBasic>().Owner == -1)
+                {
+                    obj.GetComponent<Collider2D>().enabled = true;
+                    obj.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+                    obj.layer = LayerMask.NameToLayer("Object");
+                    obj.GetComponent<Rigidbody2D>().AddForce(5 * (col.gameObject.transform.position - center).normalized, ForceMode2D.Impulse);
+                    obj.GetComponent<ObjectBasic>().Flying = true;
+                    obj.GetComponent<ObjectBasic>().Owner = -1;
+                }
             }
 
             if (obj.tag == "Player")
